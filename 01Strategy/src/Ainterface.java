fd4964f853b0b94bb8acbf6c2871a188d82e8677
kimgeuntae <kok617@naver.com>
2020-01-02 18:28:58 +0900

    Add [Strategy Pattern]

---
+++ 01Strategy/src/Ainterface.java
@@ -0,0 +1,7 @@
+
+public interface Ainterface {
+
+	// 기능에 선언
+	public void funcA();
+	
+}
---
+++ 01Strategy/src/Main.java
@@ -0,0 +1,14 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		Ainterface ainterface = new AinterfaceImpl();
+		
+		// ≈Î∑Œ
+//		ainterface.funcA();
+		
+		AObj aObj = new AObj();
+		aObj.funcAA();
+		
+	}
+}
